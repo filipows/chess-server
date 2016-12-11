7d461c84897e6204fd8a7717db9e02ad64fb31fd
Chris <chris@example.com>
2016-12-11 23:22:17 +0100

    small refactor

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ var net = require('net');
 
 const connections = [];
 
-var server = net.createServer((conn) => {
-    connections.push(conn);
-    const remoteAddress = conn.remoteAddress + ':' + conn.remotePort; 
+var server = net.createServer((currentConn) => {
+    connections.push(currentConn);
+    const remoteAddress = currentConn.remoteAddress + ':' + currentConn.remotePort; 
     console.log('connected: ' + remoteAddress);
-    conn.write('Welcome to chat. No of connected clients: ' + connections.length + '\n');
+    currentConn.write('Welcome to chat. No of connected clients: ' + connections.length + '\n');
 
-    conn.setEncoding('utf8');
+    currentConn.setEncoding('utf8');
 
-    conn.on('data', (data) => {
+    currentConn.on('data', (data) => {
+        console.log('data: ', data);
         console.log('connection data from %s: %j', remoteAddress, data);
         connections.forEach( (c) => {
-            if (conn != c) {
+            if (currentConn != c) {
                 c.write(data);  
             }
         });
     });
 
-    conn.once('close', () => {
+    currentConn.once('close', () => {
         console.log('connection from %s closed', remoteAddress);
     });
 
-    conn.on('error', (err) => {
+    currentConn.on('error', (err) => {
         console.log('connection %s error: %s', remoteAddress, err.message);
     });
 
-    conn.on('end', () => {
+    currentConn.on('end', () => {
       console.log('client disconnected');
-      let idx = connections.findIndex( (c) => c == conn);
-      connections.splice(idx, 1);
+      connections.splice(connections.indexOf(currentConn), 1);
   });
 });
 
